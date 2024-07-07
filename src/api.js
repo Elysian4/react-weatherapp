67f2fff748db3bc410baac80400e52d002873079
Elysian4 <niyati.4gupta@gmail.com>
2024-07-07 22:13:56 +0530

    adde search city api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const Url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'bb960b5517msh9988e3d68e3f943p1acd98jsn8f554046daa7',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
